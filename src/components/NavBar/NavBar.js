9f5c057984ea3051e8a15b9ae0e3fe176a655e56
Kacper <k4cperwi3lgosz@gmail.com>
2023-09-30 17:24:22 +0100

    Delete main branch, move paintings to components

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
+import styles from './NavBar.module.scss'
 
 const NavBar = () => {
     return(
         <> 
-        <ul>
-            <li>
+        <ul className={styles.Navbar}>
+            <li className={styles.list}>
                 <Link to="/" >Home</Link>
             </li>
-            <li>
+            <li className={styles.list}>
                 <Link to="/about">About</Link>
             </li>
-            <li>
+            <li className={styles.list}>
                 <Link to="/contact">Contact</Link>
             </li>
         </ul>
